294014aec7394325b63e069265f6c9ac77c5a00f
AlphaOwl <98154867+AlphaOwlGit@users.noreply.github.com>
2024-10-05 21:23:44 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement('button');
+button.innerHTML = 'Click this to make Christmas come faster 🎅';
+app.append(button);
