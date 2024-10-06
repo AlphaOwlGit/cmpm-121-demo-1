fe7ff45d5e276e43b452915b0925cfaf56230692
AlphaOwl <98154867+AlphaOwlGit@users.noreply.github.com>
2024-10-06 05:55:20 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter = 0;
 const button = document.createElement("button");
 button.innerHTML = "Click this to make Christmas come faster 🎅";
+button.addEventListener('click', () => {
+    counter++;
+    button.innerHTML = "You want Christmas to come faster " + counter + " times 🎅";
+})
 app.append(button);
